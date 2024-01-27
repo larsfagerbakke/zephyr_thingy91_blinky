65b1e8f3f38aac637b42ce32d30408e23436428e
Lars Fagerbakke <lars@fagerbakke.net>
2024-01-27 17:55:32 +0100

    Added logging

--- src/main.c
+++ src/main.c
@@ -1,6 +1,18 @@
 #include <zephyr/kernel.h>
 
+#include <zephyr/logging/log.h>
+
+#define MODULE main
+LOG_MODULE_REGISTER(MODULE, LOG_LEVEL_DBG);
+
 int main(void)
 {
+        LOG_INF("Application starting...");
+
+        while (1)
+        {
+                k_msleep(1000);
+        }
+
         return 0;
 }
